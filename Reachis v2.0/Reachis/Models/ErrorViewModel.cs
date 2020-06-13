16a03ce881ec208643ac51cb108741445c2f158a
Lika <shelley.bass.li@gmail.com>
2020-06-13 11:52:44 +0300

    Create web project.

---
+++ Reachis v2.0/Reachis/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Reachis.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
